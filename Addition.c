5d0dca63963736532102ce655054a89e5df7727e
vaib2000 <100780455+vaib2000@users.noreply.github.com>
2022-03-02 19:32:19 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1,num2,ans;                 //declaration
+	
+	num1=10;
+	num2=20;                           //initialization
+	
+	ans=num1+num2;                     //operation/logic
+	
+	printf("%d",ans);                  //output
+	return 0;
+}
